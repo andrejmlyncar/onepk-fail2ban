675c81ffe7faeadaba347674dadaf7b15150aae8
Andrej Mlyncar <a.mlyncar@gmail.com>
2016-11-25 11:50:54 +0100

    Added config to make connection working. added sample test with listing of interfaces to check if features are accessible.

--- src/main/java/sk/fiit/aks/fail2ban/controller/ConnectionFactory.java
+++ src/main/java/sk/fiit/aks/fail2ban/controller/ConnectionFactory.java
@@ -4,14 +4,14 @@ import com.cisco.onep.core.exception.OnepConnectionException;
 import com.cisco.onep.core.exception.OnepDuplicateElementException;
 import com.cisco.onep.core.exception.OnepIllegalArgumentException;
 import com.cisco.onep.core.exception.OnepInvalidSettingsException;
+import com.cisco.onep.core.util.TLSUnverifiedElementHandler;
 import com.cisco.onep.element.NetworkApplication;
 import com.cisco.onep.element.NetworkElement;
+import com.cisco.onep.element.SessionConfig;
 import com.cisco.onep.element.SessionHandle;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import sk.fiit.aks.fail2ban.exception.Fail2banConnectionException;
 
 /**
@@ -30,7 +30,7 @@ public class ConnectionFactory {
             NetworkApplication networkApplication = NetworkApplication.getInstance();
             NetworkElement networkElement = networkApplication.getNetworkElement(ipAddress);
             networkApplication.setName(name);
-            SessionHandle handle = networkElement.connect(username, password);
+            SessionHandle handle = networkElement.connect(username, password, createSessionConfig());
             elements.add(networkElement);
             return handle;
         } catch (OnepConnectionException ex) {
@@ -53,5 +53,29 @@ public class ConnectionFactory {
             }
         }
         throw new Fail2banConnectionException("Unable to obtain connection with name " + name + " connection was not initialized");
+    }
+
+    private SessionConfig createSessionConfig() {
+        SessionConfig config = null;
+        config = new SessionConfig(SessionConfig.SessionTransportMode.TLS);
+        config.setPort(SessionConfig.DEFAULT_PORT);
+        config.setReconnectTimer(10);
+        config.setEventQueueSize(100);
+        config.setEventThreadPool(25);
+        config.setReconnectTimer(25);
+        config.setEventDropMode(SessionConfig.DEFAULT_EVENT_DROP_MODE);
+        config.setKeepAliveIdleTime(SessionConfig.DEFAULT_KEEPALIVE_IDLE_TIME);
+        config.setKeepAliveInterval(SessionConfig.DEFAULT_KEEPALIVE_INTERVAL);
+        config.setKeepAliveRetryCount(SessionConfig.DEFAULT_KEEPALIVE_RETRY_COUNT);
+        config.setTLSPinning("C:\\Users\\Andrej\\Desktop\\cacert2\\pinfile", new PinningHandler());
+        return config;
+    }
+
+    public class PinningHandler implements TLSUnverifiedElementHandler {
+
+        @Override
+        public TLSUnverifiedElementHandler.Decision handleVerify(String host, String hashType, String fingerprint, boolean changed) {
+            return TLSUnverifiedElementHandler.Decision.ACCEPT_AND_PIN;
+        }
     }
 }
--- src/test/java/sk/fiit/aks/fail2ban/test/ConnectionTest.java
+++ src/test/java/sk/fiit/aks/fail2ban/test/ConnectionTest.java
@@ -5,9 +5,14 @@
  */
 package sk.fiit.aks.fail2ban.test;
 
+import com.cisco.onep.core.exception.OnepConnectionException;
+import com.cisco.onep.core.exception.OnepRemoteProcedureException;
 import com.cisco.onep.element.SessionHandle;
+import com.cisco.onep.interfaces.NetworkInterface;
+import java.util.List;
 import org.junit.Test;
 import sk.fiit.aks.fail2ban.controller.ConnectionFactory;
+import sk.fiit.aks.fail2ban.controller.InterfaceManager;
 import sk.fiit.aks.fail2ban.exception.Fail2banConnectionException;
 
 /**
@@ -17,11 +22,19 @@ import sk.fiit.aks.fail2ban.exception.Fail2banConnectionException;
 public class ConnectionTest {
 
     @Test
-    public void basicConnectionTest() {
+    public void basicConnectionTest() throws OnepRemoteProcedureException, OnepConnectionException {
         ConnectionFactory factory = new ConnectionFactory();
         SessionHandle handle;
         try {
-            handle = factory.createConnection("kokot", "kokot", "192.168.132.5", "csr_router1");
+            handle = factory.createConnection("cisco", "cisco", "192.168.132.7", "csr_router1");
+            InterfaceManager interfaceManager = new InterfaceManager();
+
+            List<NetworkInterface> interfaces = interfaceManager.getAllInterfaces(handle.getNetworkElement());
+            for( NetworkInterface interf :interfaces){
+                System.out.println(interf.getName());
+                System.out.println(interf.getAddressList().get(0).getHostAddress());
+            }
+            
         } catch (Fail2banConnectionException ex) {
             System.out.println("Application terminated " + ex.getMessage());
         }
