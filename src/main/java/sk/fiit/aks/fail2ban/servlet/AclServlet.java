b78118c79c1099d0427444913cabf800b85d87ac
Andrej Mlyncar <a.mlyncar@gmail.com>
2016-11-25 22:44:40 +0100

    ACL servlet logic.

--- src/main/java/sk/fiit/aks/fail2ban/servlet/AclServlet.java
+++ src/main/java/sk/fiit/aks/fail2ban/servlet/AclServlet.java
@@ -1,13 +1,20 @@
 package sk.fiit.aks.fail2ban.servlet;
 
 // Import required java libraries
+import com.cisco.onep.policy.L3Ace;
+import com.cisco.onep.policy.L3Acl;
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.json.Json;
 import javax.json.JsonObjectBuilder;
 import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import sk.fiit.aks.fail2ban.controller.ElementRegistry;
+import sk.fiit.aks.fail2ban.exception.AccessListManagerException;
+import sk.fiit.aks.fail2ban.exception.Fail2banConnectionException;
 
 public class AclServlet extends HttpServlet {
 
@@ -17,9 +24,20 @@ public class AclServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("application/json");
-        response.setStatus(200);
 
+        String id = request.getParameter("router_id");
+        try {
+            L3Acl accessList = ElementRegistry.getInstance().getRouter(id).getAccessListManager().getAccessList();
+            response.setContentType("application/json");
+            response.setStatus(200);
+            for(L3Ace ace : accessList.getAceList()) {
+                
+            }
+            
+        } catch (Fail2banConnectionException | AccessListManagerException ex) {
+            response.setStatus(500);
+            response.getWriter().write(ex.toString());
+        } 
         JsonObjectBuilder objectBuilder;
         objectBuilder = Json.createObjectBuilder()
                 .add("data", "Obtaining access lists");
