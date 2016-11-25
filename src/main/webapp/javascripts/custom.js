aabe99d04945dcb0e02601787980aad5ca41afb8
Andrej Mlyncar <a.mlyncar@gmail.com>
2016-11-25 19:30:50 +0100

    Added tables to view.

---
+++ src/main/webapp/javascripts/custom.js
@@ -0,0 +1,10 @@
+
+function displayRouters() {
+    $("#acl-table").hide();
+    $("#router-table").show();
+}
+
+function displayAclTable() {
+    $("#router-table").hide();
+    $("#acl-table").show();
+}
