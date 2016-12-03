3d3e6ba374d72e682ff5056af05c07b29751d715
Andrej Mlyncar <a.mlyncar@gmail.com>
2016-12-03 01:00:59 +0100

    web content for acl servlet.

--- src/main/webapp/javascripts/custom.js
+++ src/main/webapp/javascripts/custom.js
@@ -1,13 +1,14 @@
 
 function displayRouters() {
-    $("#acl-table").hide();
+    $("#acl-tables").hide();
     $("#router-table").show();
     fillRoutersTable();
 }
 
 function displayAclTable() {
     $("#router-table").hide();
-    $("#acl-table").show();
+    displayAccessLists();
+    $("#acl-tables").show();
 }
 
 
@@ -54,4 +55,31 @@ function apiCallPost(call_name, post_data, callback) {
 
 function routerShowModal() {
     $("#router-add-modal").modal('show');
+}
+
+function displayAccessLists() {
+    var aclTablesContent = $("#acl-tables");
+    aclTablesContent.html("");
+    aclTablesContent.append("<h2>Access Lists</h2>");
+    apiCallGet('./accesslists', null, function (data) {
+        for (var i = 0; i < data.length; i++) {
+            aclTablesContent.append("<h4>Router ID: " + data[i].router_id + "</h4>");
+            aclTablesContent.append("<table class=\"table table-striped\">" +
+                    "<thead>" +
+                    "<tr> " +
+                    "<th>Sequence Number</th>" +
+                    "<th>Permit</th>" +
+                    "<th>Source</th>" +
+                    "<th>Destination</th>" +
+                    "</tr>" +
+                    "</thead>" +
+                    "<tbody id='body-"+data[i].router_id+"'>");
+
+            for (var j = 0; j < data[i].aces.length; j++) {
+                $("#body-" + data[i].router_id).append("<tr><td>" + data[i].aces[j].sequence + "</td><td>" + data[i].aces[j].permit + "</td><td>" + data[i].aces[j].source + "</td><td>" + data[i].aces[j].destination + "</td></tr>");
+            }
+            aclTablesContent.append("</tbody></table>");
+        }
+    });
+
 }
